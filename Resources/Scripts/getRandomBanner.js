71e16934626c109abab1e4309f14130e9bb8c468
leokang123 <leokang123@github.com>
2024-08-04 21:48:45 +0900

    vault backup: 2024-08-04 21:48:45

--- Resources/Scripts/getRandomBanner.js
+++ Resources/Scripts/getRandomBanner.js
@@ -1,12 +1,13 @@
 const getRandomBanner = (input) => {
-  // const fs = require('fs');
+  const fs = require('fs');
   // const path = require('path');
-  const obsidianPath = '/Users/jeonghun/Documents/Obsidian Vault';
-  const backGroundPath = path.join(obsidianPath, 'Resources', 'Background');
-  const resourceArr = fs.readdirSync(backGroundPath);
-  const resourceLength = resourceArr.length;
-  const randomNumber = Math.floor(Math.random() * resourceLength);
-  return `\"![[${resourceArr[randomNumber]}]]\"`;
+  // const obsidianPath = '/Users/jeonghun/Documents/Obsidian Vault';
+  // const backGroundPath = path.join(obsidianPath, 'Resources', 'Background');
+  // const resourceArr = fs.readdirSync(backGroundPath);
+  // const resourceLength = resourceArr.length;
+  // const randomNumber = Math.floor(Math.random() * resourceLength);
+  // return `\"![[${resourceArr[randomNumber]}]]\"`;
+  return '123';
 };
 
 module.exports = getRandomBanner;
