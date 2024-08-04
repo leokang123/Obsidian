5bcb83a46bcc6262bdfc066ce0ac52a26b658a1b
leokang123 <leokang123@github.com>
2024-08-04 22:38:54 +0900

    vault backup: 2024-08-04 22:38:54

--- Resources/Scripts/makeLogData.js
+++ Resources/Scripts/makeLogData.js
@@ -1,8 +1,9 @@
 const makeLogData = (input) => {
   const fs = require('fs');
-  const date = new Date();
-  const fullDate = `${date.getFullYear()}-${date.getMonth()}`;
-  console.log(date);
+  const seoulTime = new Date().toLocaleString('kr', {
+    timeZone: 'Asia/Seoul',
+  });
+  console.log(seoulTime);
 };
 
 module.exports = makeLogData;
