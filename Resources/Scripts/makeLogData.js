9b6f475eab9a3482e2963da44194cc3f3be83739
leokang123 <leokang123@github.com>
2024-08-04 23:52:45 +0900

    vault backup: 2024-08-04 23:52:45

--- Resources/Scripts/makeLogData.js
+++ Resources/Scripts/makeLogData.js
@@ -3,14 +3,15 @@ const makeLogData = (filePath) => {
   const path = require('path');
   const obsidianPath = '/Users/jeonghun/Documents/Obsidian Vault';
   const logPath = path.join(obsidianPath, 'Resources/Log/log.md');
-  const seoulTime = new Date().toLocaleString('kr', {
-    timeZone: 'Asia/Seoul',
-    hour12: false,
-  });
+  const date = new Date();
   const relativeFilePath = path.relative(obsidianPath, filePath);
+  const seoulTime = `${date.getFullYear()}-${date.getMonth().padStart(2, '0')}-${date.getDate().padStart(2, '0')} ${date.getHours().padStart(2, '0')}:${date.getMinutes().padStart(2, '0')}:${date.getSeconds().padStart(2, '0')}`;
   const logData = `[${seoulTime}] ${relativeFilePath} 생성\n`;
   fs.appendFileSync(logPath, logData);
   console.log(`${logPath}에 로그 기록 완료`);
 };
+const date = new Date();
 
+const seoulTime = `${date.getFullYear()}-${date.getMonth().padStart(2, '0')}-${date.getDate().padStart(2, '0')} ${date.getHours().padStart(2, '0')}:${date.getMinutes().padStart(2, '0')}:${date.getSeconds().padStart(2, '0')}`;
+console.log(seoulTime);
 module.exports = makeLogData;
