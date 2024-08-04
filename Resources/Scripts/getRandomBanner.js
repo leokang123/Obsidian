17c9a053810178e2e5ff698d9cf096722fd4a4da
leokang123 <leokang123@github.com>
2024-08-04 21:48:00 +0900

    vault backup: 2024-08-04 21:48:00

--- Resources/Scripts/getRandomBanner.js
+++ Resources/Scripts/getRandomBanner.js
@@ -1,6 +1,6 @@
 const getRandomBanner = (input) => {
-  const fs = require('fs');
-  const path = require('path');
+  // const fs = require('fs');
+  // const path = require('path');
   const obsidianPath = '/Users/jeonghun/Documents/Obsidian Vault';
   const backGroundPath = path.join(obsidianPath, 'Resources', 'Background');
   const resourceArr = fs.readdirSync(backGroundPath);
