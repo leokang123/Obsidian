484cdfc48468c1f316fa123b83227a4deff9c361
leokang123 <leokang123@github.com>
2024-08-04 22:22:45 +0900

    vault backup: 2024-08-04 22:22:45

--- Resources/Scripts/getSeriesFromTitle.js
+++ Resources/Scripts/getSeriesFromTitle.js
@@ -3,7 +3,7 @@ const getSeriesFromTitle = (input) => {
   const strArr = str.split(' ');
   const lastElement = strArr[strArr.length - 1].match(/\d+/)[0];
   if (lastElement === null) return 1;
-  return Number(lastElement) + 1;
+  return Number(lastElement);
 };
 
 module.exports = getSeriesFromTitle;
