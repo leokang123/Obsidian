0222a4494e8753f07e99f22af2ec4cc6fb68a8d6
leokang123 <leokang123@github.com>
2024-08-04 21:18:11 +0900

    vault backup: 2024-08-04 21:18:11

--- Resources/Scripts/getRandomBanner.js
+++ Resources/Scripts/getRandomBanner.js
@@ -1,13 +1,14 @@
-const getRandomBanner = () => {
-  const fs = require("fs");
-  const path = require("path");
-  const parentDir = path.resolve(__dirname, "");
-  const backGroundPath = path.join(parentDir, "Background");
-  return fs.existsSync(backGroundPath);
-  // const resourceLength = resourceArr.length;
-  // const randomNumber = Math.floor(Math.random() * resourceLength);
-  // return `![[${resourceArr[randomNumber]}]]`;
-  return resourceArr;
+const getRandomBanner = (input) => {
+  //   const fs = require("fs");
+  //   const path = require("path");
+  console.log(input);
+  //   const parentDir = path.resolve(input, "..");
+  //   const backGroundPath = path.join(parentDir, "Background");
+  //   return parentDir;
+  //   // const resourceLength = resourceArr.length;
+  //   // const randomNumber = Math.floor(Math.random() * resourceLength);
+  //   // return `![[${resourceArr[randomNumber]}]]`;
+  //   return resourceArr;
 };
 module.exports = getRandomBanner;
-console.log(getRandomBanner());
+// console.log(getRandomBanner());
---
+++ Studied/asd asd 1.md
@@ -0,0 +1,12 @@
+---
+주제: Default
+cssclasses: wide-page
+생성일: 2024-08-04 21:11
+수정일: 2024년 08월 04일 오후 21시 01분
+series: 2
+---
+
+# Default
+#Default
+
+false
