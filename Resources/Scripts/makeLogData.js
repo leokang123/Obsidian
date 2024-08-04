cf142ae18e21f2bc7df427a62b8a92cbdffe641d
leokang123 <leokang123@github.com>
2024-08-04 22:42:23 +0900

    vault backup: 2024-08-04 22:42:22

--- Resources/Scripts/makeLogData.js
+++ Resources/Scripts/makeLogData.js
@@ -1,9 +1,14 @@
 const makeLogData = (input) => {
   const fs = require('fs');
+  const path = require('path');
+  const obsidianPath = '/Users/jeonghun/Documents/Obsidian Vault';
+  const logPath = path.join(obsidianPath, 'Resources/Log/log.md');
   const seoulTime = new Date().toLocaleString('kr', {
     timeZone: 'Asia/Seoul',
   });
-  console.log(seoulTime);
+  const logData = `[${seoulTime}] ${input} 생성`;
+  fs.appendFileSync(logPath, logData);
+  console.log(`${logPath}에 로그 기록 완료`);
 };
 
 module.exports = makeLogData;
