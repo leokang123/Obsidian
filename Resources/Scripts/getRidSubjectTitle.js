14f79c4be5659ae10554707a6d94fe57fb6e4964
leokang123 <leokang123@github.com>
2024-08-04 22:12:25 +0900

    vault backup: 2024-08-04 22:12:25

--- Resources/Scripts/getRidSubjectTitle.js
+++ Resources/Scripts/getRidSubjectTitle.js
@@ -2,7 +2,7 @@ const getRidSubjectTitle = (input) => {
   const str = input.trim();
   const matchArr = str.match(/\((.*)\)/);
   if (matchArr === null) return 'Default';
-  const title = str.substring(matchStr[0].length);
+  const title = str.substring(matchArr[0].length).trim();
   return title;
 };
 
