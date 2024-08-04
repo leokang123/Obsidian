ae71952d6563b24c2fc18d6e7f46e0768470bc31
leokang123 <leokang123@github.com>
2024-08-04 21:02:53 +0900

    vault backup: 2024-08-04 21:02:53

--- Resources/Scripts/getRandomBanner.js
+++ Resources/Scripts/getRandomBanner.js
@@ -1,12 +1,13 @@
 const getRandomBanner = () => {
   const fs = require("fs");
   const path = require("path");
-  const parentDir = path.resolve(__dirname, "../..");
+  const parentDir = path.resolve(__dirname, "");
   const backGroundPath = path.join(parentDir, "Background");
-  const resourceArr = fs.readdirSync(__dirname);
+  return fs.existsSync(backGroundPath);
   // const resourceLength = resourceArr.length;
   // const randomNumber = Math.floor(Math.random() * resourceLength);
   // return `![[${resourceArr[randomNumber]}]]`;
   return resourceArr;
 };
 module.exports = getRandomBanner;
+console.log(getRandomBanner());
