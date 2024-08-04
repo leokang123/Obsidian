b1329c47431678b209f6e5932b150cd05ae63be1
leokang123 <leokang123@github.com>
2024-08-04 19:12:04 +0900

    vault backup: 2024-08-04 19:12:04

---
+++ Resources/Scripts/myFunction.js
@@ -0,0 +1,10 @@
+const makeSubject = (input) => {
+  const str = input;
+  const strArr = str.split(' ');
+  const matchStr = strArr[0].match(/\((.*)\)/);
+  return matchStr ? matchStr[1] : 'Default';
+};
+
+module.exports = {
+  makeSubject;
+}
