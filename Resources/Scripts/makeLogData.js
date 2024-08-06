226ea03b068394c8099eae672f6179aa9f4753df
leokang123 <leokang123@github.com>
2024-08-06 13:27:07 +0900

    vault backup: 2024-08-06 13:27:07

--- Resources/Scripts/makeLogData.js
+++ Resources/Scripts/makeLogData.js
@@ -6,10 +6,10 @@ const makeLogData = (filePath) => {
   const date = new Date();
   const relativeFilePath = path.relative(obsidianPath, filePath);
   const months = String(date.getMonth() + 1).padStart(2, '0');
-  const dates = String(date.getDate() + 1).padStart(2, '0');
-  const hours = String(date.getHours() + 1).padStart(2, '0');
-  const minutes = String(date.getMinutes() + 1).padStart(2, '0');
-  const seconds = String(date.getSeconds() + 1).padStart(2, '0');
+  const dates = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
   const seoulTime = `${date.getFullYear()}-${months}-${dates} ${hours}:${minutes}:${seconds}`;
   const logData = `[${seoulTime}] ${relativeFilePath} 생성\n`;
   fs.appendFileSync(logPath, logData);
---
+++ Resources/Templates/Task.md
@@ -0,0 +1,26 @@
+---
+created: <% tp.date.now("yyyy년 MM월 DD일 a HH시 mm분") %>
+수정일:  2024년 08월 06일 오후 12시 47분
+banner: "![[yort.jpg]]"
+banner_y: 0.088
+---
+
+<%* tp.user.makeLogData(tp.file.path(), tp.file.title) %>
+#데일리노트
+
+# {{date}}
+
+- [ ] TBU
+- [ ] TBU
+
+[[<% tp.date.yesterday("YYYY-MM-DD") %>|< 전날]] | [[<% tp.date.tomorrow("YYYY-MM-DD") %>|다음날 >]]
+
+---
+
+## 일감 감옥
+
+```tasks  
+path includes task  
+not done  
+short mode  
+```
