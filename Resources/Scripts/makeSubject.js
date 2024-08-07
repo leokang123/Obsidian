eb4b5d3482bc99dae9d7aaef0f9f62ba25051be4
leokang123 <leokang123@github.com>
2024-08-07 14:51:55 +0900

    vault backup: 2024-08-07 14:51:55

--- Resources/Scripts/makeSubject.js
+++
@@ -1,8 +0,0 @@
-const makeSubject = (input) => {
-  const str = input;
-  const strArr = str.split(' ');
-  const matchStr = strArr[0].match(/\((.*)\)/);
-  return matchStr ? matchStr[1] : 'Default';
-};
-
-module.exports = makeSubject;
---
+++ Studied/(asd) asd 2.md
@@ -0,0 +1,15 @@
+---
+주제: asd
+cssclasses: wide-page
+생성일: 2024년 08월 07일 오후 14시 46분
+수정일:  2024년 08월 05일 오전 06시 28분
+series: 2
+banner: "![[sunsetSea.jpg]]"
+banner_y: 0.6
+---
+
+
+
+# asd 2
+
+#asd
