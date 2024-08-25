0f03a0cd98f9ace50c8fe0e7217e13ec6776ebb9
leokang123 <leokang123@github.com>
2024-08-26 04:32:26 +0900

    vault backup: 2024-08-26 04:32:26

--- .trash/getRidSubjectTitle.js
+++
@@ -1,9 +0,0 @@
-const getRidSubjectTitle = (input) => {
-  const str = input.trim();
-  const matchArr = str.match(/\((.*)\)/g);
-  if (matchArr === null) return 'Default';
-  const title = str.substring(matchArr[0].length).trim();
-  return title;
-};
-
-module.exports = getRidSubjectTitle;
