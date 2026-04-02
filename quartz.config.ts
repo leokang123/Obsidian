038674b6ca5b55fb86f35615594100db6943e9e0
leokang123 <leokang123@github.com>
2026-04-02 23:12:11 +0900

    created date order

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     locale: "ko-KR",
     baseUrl: "leokang123.github.io/Obsidian",
     ignorePatterns: [".obsidian"],
-    defaultDateType: "date",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
